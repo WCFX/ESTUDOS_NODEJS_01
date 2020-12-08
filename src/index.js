abf26be8283222f94af174f73fa3db808eba6c59
wcfx <wcfx.dev@gmail.com>
2020-12-08 18:22:42 -0300

    Estudos com backend 01

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require ('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -33,6 +33,15 @@ function logRequest(req, res, next){
 
   return next() // Próximo Middleware
 }
+
+function validateProjectId(req, res, next){
+  const { id } = req.params;
+
+  if(!isUuid(id)){
+    return res.status(400).json({ error: 'Invalid project ID.'})
+  }
+  return next();
+}
 app.use(logRequest)
 
 app.get('/projects', (req, res) => {
@@ -54,7 +63,8 @@ app.post('/projects', (req, res) => {
   
   return res.json(project)
 });
-app.put('/projects/:id', (req, res) => {
+
+app.put('/projects/:id', validateProjectId, (req, res) => {
 
   const { id } = req.params;
   const { title, owner } = req.body
@@ -73,7 +83,8 @@ app.put('/projects/:id', (req, res) => {
 
   return res.json(project)
 });
-app.delete('/project/:id', (req, res) => {
+
+app.delete('/project/:id', validateProjectId, (req, res) => {
   const { id } = req.params;
 
   const projectIndex = projects.findIndex(project => project.id === id)
@@ -84,5 +95,6 @@ app.delete('/project/:id', (req, res) => {
 
   return res.status(204).send()
 });
+
 app.listen(3333);
 
